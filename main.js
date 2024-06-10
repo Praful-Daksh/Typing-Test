49fdc596e336538c7820c69b6bc8ca57ca4c2447
Praful-Daksh <prafuldaksh@proton.me>
2024-06-10 14:37:36 +0530

    update

--- main.js
+++ main.js
@@ -4,12 +4,96 @@ var HalfMin = document.querySelector('#halfMin');
 var Min = document.querySelector('#Min');
 var quarter = document.querySelector('#quarter');
 var Mistakes = document.querySelector('#mistakes');
-errors = 0;
+var typingLetters;
+var errors = 0;
 var time = 30;
 var activeCount = HalfMin;
 var letters;
-var isTyping = false;
 let letterIndex = 0;
+
+// generates random paragraph
+function randomPara() {
+  var indexRandom = Math.floor(Math.random() * paragraphs.length);
+  letters = paragraphs[indexRandom].split("");
+  var fragment = document.createDocumentFragment();
+  for (let i = 0, len = letters.length; i < len; i++) {
+    let spanTag = document.createElement('span');
+    spanTag.textContent = letters[i];
+    fragment.appendChild(spanTag);
+  }
+  paragraphText.appendChild(fragment);
+}
+
+// when user starts typing
+function startTyping() {
+   typingLetters = paragraphText.children;
+  var typedLetter = inputArea.value[letterIndex];
+  if (typedLetter == null) {
+    if (letterIndex > 0) {
+      if (typingLetters[--letterIndex].classList.contains('wrong')) {
+        errors--;
+      }
+      typingLetters[letterIndex].classList.remove('correct', 'wrong');
+    }
+  } else {
+    typingLetters[letterIndex].classList.add('blink');
+    if (typingLetters[letterIndex].textContent === typedLetter) {
+      typingLetters[letterIndex].classList.add('correct');
+    } else {
+      typingLetters[letterIndex].classList.add('wrong');
+      errors++;
+    }
+    letterIndex++;
+    
+  }
+  Mistakes.textContent = errors;
+}
+
+// starts timer
+let countdown;
+
+function startTimer() {
+  clearRemain();
+  countdown = setInterval(() => {
+    time--;
+    activeCount.textContent = time;
+    if (time <= 0) {
+      clearInterval(countdown);
+      inputArea.disabled = true;
+    }
+  }, 1000);
+}
+
+// displays the count that user selected
+function clearRemain() {
+  if (activeCount == HalfMin) {
+    Min.className = 'display';
+    quarter.className = 'display';
+  }
+  if (activeCount == Min) {
+    halfMin.className = 'display';
+    quarter.className = 'display';
+  }
+  if (activeCount == quarter) {
+    Min.className = 'display';
+    halfMin.className = 'display';
+  }
+}
+
+// event listeners
+inputArea.addEventListener("keyup",()=>{
+  if(letterIndex>0){
+    typingLetters[letterIndex-1].classList.remove('blink');
+  }
+  startTyping();
+  
+});
+
+inputArea.addEventListener('keydown', () => {
+  if (!countdown) {
+    startTimer();
+  }
+});
 
 HalfMin.addEventListener('click', () => {
   activeCount = HalfMin;
@@ -32,93 +116,4 @@ quarter.addEventListener('click', () => {
   Min.classList.remove('time-active');
   quarter.className = 'time-active';
 });
-
-
-
-
-
 randomPara();
-
-// generates random paragraph
-
-function randomPara() {
-  var indexRandom = Math.floor(Math.random() * paragraphs.length);
-  letters = paragraphs[indexRandom].split("");
-  for (let i = 0; i < letters.length; i++) {
-    let spanTag = `<span>${letters[i]}</span>`;
-    paragraphText.innerHTML += spanTag;
-  }
-}
-// when user starts typing
-
-function startTyping() {
-  var typingLetters = document.querySelectorAll('span');
-  var typedLetter = inputArea.value.split("")[letterIndex];
-
-  if (typedLetter == null) {
-    if (letterIndex > 0) {
-        if (typingLetters[letterIndex-1].classList.contains('wrong')) {
-            errors--;
-        }
-        letterIndex--; 
-        typingLetters[letterIndex].classList.remove("correct", "wrong");
-    }
-}
-
-  else {
-    if (typingLetters[letterIndex].textContent == typedLetter) {
-      typingLetters[letterIndex].classList.add('correct');
-    }
-    else {
-      typingLetters[letterIndex].classList.add('wrong');
-      errors++;
-    }
-    letterIndex++;
-  }
-  Mistakes.textContent = errors;
-}
-
-// starts timer
-let countdown;
-
-function startTimer() {
-  clearRemain();
-  countdown = setInterval(() => {
-    time--;
-    activeCount.textContent = time;
-    if (time <= 0) {
-      clearInterval(countdown);
-      inputArea.disabled = true;
-    }
-  }, 1000);
-}
-
-// displays the count that user selected
-
-function clearRemain() {
-  if (activeCount == HalfMin) {
-    Min.className = 'display';
-    quarter.className = 'display';
-  }
-  if (activeCount == Min) {
-    halfMin.className = 'display';
-    quarter.className = 'display';
-  }
-  if (activeCount == quarter) {
-    Min.className = 'display';
-    halfMin.className = 'display';
-  }
-}
-
-
-
-// events listener s
-
-
-inputArea.addEventListener("keyup", startTyping);
-
-inputArea.addEventListener('keydown', () => {
-  if (!countdown) {
-    startTimer();
-  }
-})
